f79e028d210e1eeae9502c4c92d82194cec42410
Giseli Matos <giseli3690@gmail.com>
2026-06-27 17:06:04 -0300

    correções do login com google

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/controller/UserController.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -20,10 +21,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import br.edu.utfpr.pb.pw44s.server.dto.GoogleAuthRequest;
+import br.edu.utfpr.pb.pw44s.server.security.SecurityConstants;
+import br.edu.utfpr.pb.pw44s.server.security.dto.AuthenticationResponse;
+import br.edu.utfpr.pb.pw44s.server.security.dto.UserResponseDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import java.util.Date;
+import org.springframework.http.ResponseEntity;
+import br.edu.utfpr.pb.pw44s.server.dto.GoogleAuthRequest;
 import br.edu.utfpr.pb.pw44s.server.dto.UserDTO;
 import br.edu.utfpr.pb.pw44s.server.error.ApiError;
 import br.edu.utfpr.pb.pw44s.server.model.User;
+import br.edu.utfpr.pb.pw44s.server.security.SecurityConstants;
+import br.edu.utfpr.pb.pw44s.server.security.dto.AuthenticationResponse;
+import br.edu.utfpr.pb.pw44s.server.security.dto.UserResponseDTO;
 import br.edu.utfpr.pb.pw44s.server.service.UserService;
 import br.edu.utfpr.pb.pw44s.server.shared.GenericResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -49,6 +61,18 @@ public class UserController {
     @PreAuthorize("hasRole('ADMIN') or hasRole('OPERATOR')")
     public Page<UserDTO> listUsers(Pageable pageable) {
         return userService.findAll(pageable).map(UserDTO::new);
+    }
+
+    @PostMapping("/google-auth")
+    public ResponseEntity<AuthenticationResponse> googleAuth(@RequestBody GoogleAuthRequest request) {
+        User user = userService.findOrCreateGoogleUser(request);
+
+        String token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET));
+
+        return ResponseEntity.ok(new AuthenticationResponse(token, new UserResponseDTO(user)));
     }
 
     @PatchMapping("/{id}/ativar")
